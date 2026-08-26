2d9760930e696b2c3af25e12d589e4ea0e7ded18
AbdulrahmanShaaban <aboodxs3a@gmail.com>
2026-08-26 21:41:43 +0300

    vercel

--- backend/api/index.ts
+++ backend/api/index.ts
@@ -1,1 +1,10 @@
 export { default } from '../src/vercel';
+{
+  "$schema": "https://openapi.vercel.sh/vercel.json",
+  "rewrites": [
+    {
+      "source": "/api/:path*",
+      "destination": "/api"
+    }
+  ]
+}
